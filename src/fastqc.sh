931d1291739975d1d73e013062502c67329bc37c
Masha <kuksin.m@gmail.com>
2019-11-06 10:20:07 +0000

    fastqc petit jeu données

--- src/fastqc.sh
+++ src/fastqc.sh
@@ -1,4 +1,18 @@
 #! /bin/bash
 
+data="/home/rstudio/disk"
+cd $data
+mkdir -p fastqc_files # On crée un nouveau dossier sra_data
+cd fastqc_files
 
-fastqc -h
+FASTQ=`ls /home/rstudio/disk/sra_data *.fastq` #all the fastq files
+
+
+for file in $FASTQ # $: valeur de la variable
+do
+# Produces one fastq file, single end data.
+echo $file
+fastqc "/home/rstudio/disk/sra_data/"$file -o "/home/rstudio/disk/fastqc_files"
+
+#...
+done
