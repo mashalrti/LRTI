847e09a1d993aaf1e58eb5363e2c5a98e4e68016
Masha <maria.kuksin@ens-lyon.fr>
2019-11-06 14:32:14 +0000

    begun Trimmumatix

--- src/fastqc.sh
+++ src/fastqc.sh
@@ -5,14 +5,16 @@ cd $data
 mkdir -p fastqc_files # On crée un nouveau dossier sra_data
 cd fastqc_files
 
-FASTQ=`ls /home/rstudio/disk/sra_data *.fastq` #all the fastq files
+#FASTQ=`ls /home/rstudio/disk/sra_data/*.fastq` #all the fastq files
+FASTQ=`ls /home/rstudio/disk/sra_data/SRR330895[6-7]*.fastq` #just the two samples I added afterwards
 
 
 for file in $FASTQ # $: valeur de la variable
 do
 # Produces one fastq file, single end data.
 echo $file
-fastqc "/home/rstudio/disk/sra_data/"$file -o "/home/rstudio/disk/fastqc_files"
+#fastqc "/home/rstudio/disk/sra_data/"$file -o "/home/rstudio/disk/fastqc_files" #to do all of the files
+fastqc $file -o "/home/rstudio/disk/fastqc_files" #just the selected files
 
 #...
 done
--- src/trimmomatic.sh
+++ src/trimmomatic.sh
@@ -1,13 +1,29 @@
 #! /bin/bash
 ## Run trimmomatic
-SRR="SRR3308950
-SRR3308951
-SRR3308952
-"
+
+data="/home/rstudio/disk"
+cd $data
+mkdir -p trimmo_files
+mkdir -p paired # On va séparer les outputs paired et unpaired
+mkdir -p unpaired
+cd trimmo_files
+
+
+SRR=`ls /home/rstudio/disk/sra_data/*.fastq`
 
 for fn in $SRR;
 do
-	java -jar /softwares/Trimmomatic-0.39/trimmomatic-0.39.jar -h
+	java -jar /softwares/Trimmomatic-0.39/trimmomatic-0.39.jar \
+	  PE \
+	  -threads 8 \
+	  $fn'_1.fastq' $fn'_2.fastq' \
+	  /home/rstudio/disk/trimmo_files/paired/$fn'_paired_output_1.fastq' /home/rstudio/disk/trimmo_files/unpaired/$fn'_unpaired_output_1.fastq' \
+	  /home/rstudio/disk/trimmo_files/paired/$fn'_paired_output_2.fastq' /home/rstudio/disk/trimmo_files/unpaired/$fn'unpaired_output_2.fastq' \
+	  ILLUMINACLIP:/home/rstudio/disk/overrepresented.fa:2:30:10 \
+	  LEADING:22 \
+	  SLIDINGWINDOW:4:22 \
+	  MINLEN:25
 
 done
 
+PE [-threads <threads] [-phred33 | -phred64] [-trimlog <logFile>] <input 1> <input 2> <paired output 1> <unpaired output 1> <paired output 2> <unpaired output 2> <step 1> 
