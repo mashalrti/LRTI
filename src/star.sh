f4e1173e463f7eebed085315483c0dc358142a6f
Masha <maria.kuksin@ens-lyon.fr>
2019-11-08 09:54:42 +0000

    STAR started

--- src/star.sh
+++ src/star.sh
@@ -1,10 +1,43 @@
 #! /bin/bash
 
-#ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_32/gencode.v32.annotation.gtf.gz
-STAR --runMode genomeGenerate ...
+cd /home/rstudio/disk
+mkdir -p star/index
 
+#Liste des SRR d'intérêt
+SRR="
+SRR3308956
+SRR3308957
+SRR3308972
+SRR3308973
+SRR3308974
+SRR3308975
+"
 
-STAR ...
-samtools out.sam > out.bam
+#génération de l'index du génome humain annoté, avec 8 coeurs
+#STAR --runThreadN 8 --runMode genomeGenerate \
+#  --genomeDir star/index \
+#  --genomeFastaFiles /home/rstudio/disk/ref_transcripts/Hsap_copy_genome.fa \
+#  --sjdbGTFfile /home/rstudio/disk/ref_transcripts/Hsap_copy_annotation.gtf \
+#  --sjdbOverhang 100
 
-rm out.sam
+paired=/home/rstudio/disk/trimmo_files/paired
+
+for srr in $SRR;
+do
+#echo $srr
+#echo $paired/$srr'_paired_output_1.fastq'
+#Création d'un nouveau répertoire
+cd /home/rstudio/disk
+mkdir star/$srr'_star'
+cd star/$srr'_star'
+#Quantification des reads
+STAR --runThreadN 8 --genomeDir /home/rstudio/disk/star/index \
+  --readFilesIn $paired/$srr'_paired_output_1.fastq' \
+  $paired/$srr'_paired_output_2.fastq'
+  
+#Le fichier Aligned.out.sam est renvoyé par STAR, mais il est trop gros -> conversion en .bam, plus léger, puis suppression de
+#l'ancien fichier
+samtools view -bS -h Aligned.out.sam > $srr'.bam'
+rm Aligned.out.sam
+
+done
