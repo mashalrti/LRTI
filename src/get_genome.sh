6c45dbee519be8c293e460e499f2fb590db5453f
mashalrti <54707339+mashalrti@users.noreply.github.com>
2019-12-28 14:25:24 +0100

    Update get_genome.sh
    Vérification des derniers commentaires

--- src/get_genome.sh
+++ src/get_genome.sh
@@ -1,10 +1,10 @@
-# Ici on télécharge le génome d'Homo sapiens
+# Ici on télécharge le génome d'Homo sapiens en utilisant wget
 cd /home/rstudio/disk/ref_transcripts
 
-#Télécharge une URL, -O : output
+# Télécharge une URL, -O : output
 wget -O Hsap_annotation.gtf.gz 'ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_32/gencode.v32.annotation.gtf.gz'
 wget -O Hsap_genome.fa.gz 'https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz'
 
-#Décompression des fichiers .gz
+# Décompression des fichiers .gz
 gunzip Hsap_annotation.gtf.gz Hsap_annotation.gtf
 gunzip Hsap_genome.fa.gz Hsap_genome.fa
